bb332efe15c5e910a59ed08819f37b9721ac62fc
Iris Kuhn <ichan266@hotmail.com>
2020-10-08 13:03:52 -0700

    "AM labs - working on make_melon_type

--- harvest.py
+++ harvest.py
@@ -6,21 +6,27 @@
 class MelonType(object):
     """A species of melon at a melon farm."""
 
-    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller, 
-                 name):
+    def __init__(self, code, first_harvest, color, is_seedless, is_bestseller,  
+                 name):                                                         # initialization
         """Initialize a melon."""
-
+        self.code = code                                                        # code for instantiating MelonType
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
         self.pairings = []
 
         # Fill in the rest
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
+        self.pairings.append(pairing) # pairings from above is a list. 
 
         # Fill in the rest
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
+        self.code = new_code           # code from __inti ___
 
         # Fill in the rest
 
@@ -28,7 +34,7 @@ class MelonType(object):
 def make_melon_types():
     """Returns a list of current melon types."""
 
-    all_melon_types = []
+    all_melon_types = ["Muskmelon", "Casaba", "Crenshaw", "Yellow Watermelon"]  # a list of melon types
 
     # Fill in the rest
 
@@ -36,6 +42,10 @@ def make_melon_types():
 
 def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
+    for melon_type in melon_types:
+        print(f"{melon_type} pairs with") 
+        for pairing in parings:    
+            print(f"- {pairing}") # ??? paring info from pairing list from add_pairing function ??? 
 
     # Fill in the rest
 
